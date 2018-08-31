52aee1781a9534b99dbbd01bb58b2af741bbfa35
maximilian-gelbrecht <maximilian.gelbrecht@posteo.de>
2018-08-31 09:32:00 -0300

    integrated return code check into eval_ode_run

--- src/eval_mc_prob.jl
+++ src/eval_mc_prob.jl
@@ -15,6 +15,7 @@ import Distributions, StatsBase
 # state_filter :: array with indicies of all dimensions that should be evaluated
 # eval_funcs :: array of functions that should be applied to every dimension of the solution (except for mean and std which are always computed)
 # global_eval_funcs :: array of functions that should be applied to the complete N-dimensional solution
+# failure_handling :: How failure of integration is handles. Should be :None (do no checks), :Inf (If retcode==:DtLessThanMin: return Inf) or :Repeat (If no succes, repeat the trial (only works with random initial conditions))
 function eval_ode_run(sol, i, state_filter::Array{Int64,1}, eval_funcs::Array{<:Function,1}, global_eval_funcs::Array{<:Function,1}, failure_handling::Symbol=:None )
 
     (N_dim, N_t) = size(sol)
@@ -76,15 +77,6 @@ function eval_ode_run(sol, i)
     global_eval_funcs = [curve_entropy]
     eval_ode_run(sol, i, state_filter, eval_funcs, global_eval_funcs)
 end
-
-# include a return code check and repeat if integration failed. if this is not used with randomly generated initial conditions, this could leed into an endless loop!
-function eval_ode_run_repeat(sol, i)
-    if (sol.retcode != :Success) & (sol.retcode != :Default)
-        return ((),true)
-    end
-    eval_ode_run(sol, i )
-end
-
 
 # checks if any of the results is Inf or NaN and returns the indices in a dictionary
 function check_inf_nan(sol::myMCSol)
