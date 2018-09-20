1b940ab6d1dedc752a3890662b4cfc030f6de18a
maximilian-gelbrecht <maximilian.gelbrecht@posteo.de>
2018-09-20 19:33:06 -0300

    added sort

--- src/HighBifLib.jl
+++ src/HighBifLib.jl
@@ -2,7 +2,7 @@ module HighBifLib
 
 # export all functions declared
 export kuramoto_parameters, kuramoto, kuramoto_network_parameters, kuramoto_network, logistic_parameters, logistic, henon_parameters, henon, roessler_parameters, roessler_network, lotka_volterra, lotka_volterra_parameters
-export myMCProblem, BifAnaMCProblem, myMCSol, parameter
+export myMCProblem, BifAnaMCProblem, myMCSol, sort, sort!, parameter
 export setup_ic_par_mc_problem, define_new_problem, eval_ode_run, check_inf_nan
 export distance_matrix, distance_matrix_dense, weighted_norm
 export order_parameter
